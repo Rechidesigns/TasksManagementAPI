7119420ff72b722c88a5767609dc360fbd7b4510
Alerechi Ordu <alerechiordu@gmail.com>
2024-07-08 13:58:47 +0100

    Add project files.

---
+++ Core/Entities/Model/ApllicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TasksManagementAPI.Core.Entities.Model
+{
+    public class ApllicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAdmin { get; set; }
+
+    }
+}
